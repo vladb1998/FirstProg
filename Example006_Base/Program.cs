cf7c3c3889fbe3f5a5b81fe23b74087b73a2e372
vlad <vladbelyvskij@gmail.com>
2022-08-04 14:04:58 +0300

    BASE

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,12 @@
+﻿int A = 25;
+int B = 13;
+int C = 53;
+int D = 29;
+int E = 64;
+int max = A;
+if(B > max) max = B;
+if(C > max) max = C;
+if(D > max) max = D;
+if(E > max) max = E;
+Console.Write("max = ");
+Console.Write(max);
