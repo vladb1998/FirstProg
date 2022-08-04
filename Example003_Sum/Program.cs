d983886504535c27e5ceb17c18e232eaefe35aea
vlad <vladbelyvskij@gmail.com>
2022-08-04 13:20:48 +0300

    random sun

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,50);
+Console.WriteLine(numberB);
 Console.Write(numberA + numberB);
 
