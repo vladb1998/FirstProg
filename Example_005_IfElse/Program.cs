a02a36f4d4c687ca0addd277bba787b4e1342045
vlad <vladbelyvskij@gmail.com>
2022-08-04 13:42:40 +0300

    IfElse

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "vlad")
+{
+    Console.Write("Ура, это же Влад!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(UserName);
+}
