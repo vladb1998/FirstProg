3293517e04f0b16726a8708565dbeb054f2d48bf
vlad <vladbelyvskij@gmail.com>
2022-08-05 11:10:16 +0300

    friendsDog

---
+++ Example008_friendsDog/Program.cs
@@ -0,0 +1,28 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int time = 0;
+
+    while (distance > 10)
+    {
+        if (friend == 2)
+        {
+            time = distance / (firstFriendSpeed + dogSpeed);
+            friend = 1;
+            distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+            count += 1;
+        }
+        else
+        {
+            time = distance / (secondFriendSpeed + dogSpeed);
+            friend = 2;
+            distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+            count += 1;
+        }
+        
+    }
+    Console.Write("Собака пробежала (раз): ");
+    Console.WriteLine(count);
